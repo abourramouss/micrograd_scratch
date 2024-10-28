4f37e5039fc3f5e48d2112384b09c4e6af4509e3
Ayman <aymanbourramouss@gmail.com>
2024-10-28 12:17:29 +0100

    power, use + instead of np.add

--- micrograd.py
+++ micrograd.py
@@ -6,7 +6,7 @@ import numpy as np
 class Tensor:
     def __init__(self, data, _children=(), _op="", label=""):
         self.data = data
-        self.grad = 0.0
+        self.grad = np.zeros_like(data)
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
@@ -22,7 +22,7 @@ class Tensor:
         
         #use numpy to add two tensors
 
-        out = Tensor(np.add(self.data, other.data), (self, other), "+")
+        out = Tensor(self.data + other.data, (self, other), "+")
 
         def _backward():
             self.grad += 1 * out.grad
@@ -31,19 +31,19 @@ class Tensor:
         out._backward = _backward
         return out
 
-#     def __sub__(self, other):
-#         return self + (-other)
+    def __sub__(self, other):
+        return self + (-other)
 
 #     def __truediv__(self, other):
 #         return self * other**-1
 
-#     def __pow__(self, other):
-#         assert isinstance(other, (int, float))
-#         out = Value(self.data**other, (self,), f"**{other}")
-#         def _backward():
-#             self.grad += other * self.data**(other-1) * out.grad
-#         out._backward = _backward
-#         return out
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Tensor(self.data**other, (self,), f"**{other}")
+        def _backward():
+            self.grad += other * self.data**(other-1) * out.grad
+        out._backward = _backward
+        return out
 
 
     def __mul__(self, other):
